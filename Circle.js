12b9019a5ddc492e495c5c448dde22740c835da8
Pedro Santos <160870005+JackCipher07@users.noreply.github.com>
2024-04-28 13:03:36 +0100

    Add files via upload

---
+++ Circle.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight );
+    background(220);
+  }
+  
+  function draw() {
+    if mouse(mousex,mousey)
+      
+      then
+    circle(width/2,height/2,200)
+    
+    
+  }
